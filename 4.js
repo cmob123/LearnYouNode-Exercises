0afa7a01719a55211f07b6c1b3f7b66149ef49f8
Chris O'Brien <cobrien3@mail.smcvt.edu>
2017-06-19 12:04:49 -0400

    Update 4.js to ES6 syntax

--- 4.js
+++ 4.js
@@ -1,12 +1,12 @@
 // asynchronously print number of newline characters in the string passed (learnyounode workshopper #4)
 
-var fs = require('fs')
-var input = ""
-var array = []
+const fs = require('fs');
+let input = "",
+	array = [];
 fs.readFile(process.argv[2], 'utf8', function(err, data){
-	if(!err) input = data
-	else input = "ERROR"
-	input = data
-	var array = input.split('\n')
-	console.log(array.length-1)
-})
+	if(!err) { input = data; }
+	else input = "ERROR";
+	input = data;
+	let array = input.split('\n');
+	console.log(array.length-1);
+});
