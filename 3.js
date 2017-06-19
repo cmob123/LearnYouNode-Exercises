8e2979ae10fb666eb368a1fbbcd6921bc00ed78f
Chris O'Brien <cobrien3@mail.smcvt.edu>
2017-06-19 12:01:41 -0400

    Update 3.js to ES6 syntax

--- 3.js
+++ 3.js
@@ -1,6 +1,6 @@
 // synchronously print number of newline characters in the string passed (learnyounode workshopper #3)
 
-var fs = require('fs')
-var input = fs.readFileSync(process.argv[2]).toString()
-var array = input.split('\n')
-console.log(array.length-1)
+const fs = require('fs');
+let input = fs.readFileSync(process.argv[2]).toString();
+let array = input.split('\n');
+console.log(array.length-1);
