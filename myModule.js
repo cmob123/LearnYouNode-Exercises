b0177e38ece5c93e7973f83d3f7d9df5f9e362ce
Chris O'Brien <cobrien3@mail.smcvt.edu>
2017-06-19 14:53:06 -0400

    Improved myModule.js
    Before, i was reset to 0 every time an element was removed. Changed algorithm to instead decrement both i and len.

--- myModule.js
+++ myModule.js
@@ -6,10 +6,12 @@ module.exports = function (dirName, end, callback) {
 	if (end.charAt(0) !== '.') { end = '.' + end; } // add '.' if it's not already there
 	fs.readdir(dirName, function(err, list){
 		if (err) { return callback(err); }
-		for(let i=0; i < list.length; i += 1) {
+		let len = list.length;
+		for(let i=0; i < len; i += 1) {
 			if(list[i].endsWith(end) !== true) { // if incorrect extension is found, remove file from listing
 				list.splice(i, 1);
-				i = 0; // reset i, avoid indexing error
+				i -= 1; // decrement i, avoid indexing mismatch error
+				len -= 1;
 			}
 		}
 		callback(null, list);
