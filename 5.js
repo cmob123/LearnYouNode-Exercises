f355422b8237236e347e7e83bc6d20f8bb3bc20f
Chris O'Brien <cobrien3@mail.smcvt.edu>
2017-06-19 12:06:40 -0400

    Update 5.js to ES6 syntax

--- 5.js
+++ 5.js
@@ -1,12 +1,12 @@
-// takes a directory name and a file extension as input and returns a listing of the directory filtered with the extension (learnyounode workshopper #5)
+// takes a directory name and a file extension as input and returns a listing of the directory filtered by the extension (learnyounode workshopper #5)
 
-var fs = require('fs')
-var input = ""
-var array = []
+const fs = require('fs');
+let end = process.argv[3];
+if (end.charAt(0) !== '.') { end = '.' + end; } // add '.' if it's not already there
 fs.readdir(process.argv[2], function(err, list){
-	for(var i=0; i<list.length; i++){
-		//console.log("   " + list[i])
-		if(list[i].endsWith(process.argv[3]) && list[i].includes('.'))
-			console.log(list[i])
+	for(let i=0; i < list.length; i += 1) {
+		if(list[i].endsWith(end)) {
+			console.log(list[i]);
+		}
 	}
-})
+});
